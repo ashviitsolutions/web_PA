40f169e06166ba660b5387de43f5a7930be76e5e
unknown <munnakr686622@gmail.com>
2023-07-12 17:55:45 +0530

    update

--- src/Components/Pages/Home/Banner.js
+++ src/Components/Pages/Home/Banner.js
@@ -4,71 +4,63 @@ import { IP } from '../../../Constant';
 // import { fetchProducts, fetchImage } from '../Redux/productSlice';
 
 function Banner() {
-  // const id = '63f0cad81e627c34fc1b58e9';
-  // const [img, setImg] = useState('');
-
-
-  // const dispatch = useDispatch();
-
-  // const user = useSelector((state) => state.product.data);
+  // const postIds = ['63f0cad81e627c34fc1b58e9'];
+  // const [users, setUsers] = useState([]);
+  // const [img, setImg] = useState([]);
 
   // useEffect(() => {
-  //   dispatch(fetchProducts(id));
-  // }, [dispatch, id]);
+  //   async function fetchData() {
+  //     const cachedData = sessionStorage.getItem('cachedData');
 
-  // useEffect(() => {
-  //   if (img === '' && user && user.attachments) {
-  //     dispatch(fetchImage(user.attachments[0])).then((result) => {
-  //       setImg(result.payload);
-  //     });
+  //     if (cachedData) {
+  //       const parsedData = JSON.parse(cachedData);
+  //       setUsers(parsedData.users);
+  //       setImg(parsedData.images);
+  //     } else {
+  //       const responses = await Promise.all(
+  //         postIds.map(async (id) => {
+  //           const res = await fetch(`${IP}/post/fetch/${id}`);
+  //           return res.json();
+  //         })
+  //       );
+
+  //       setUsers(responses[0]);
+  //       const images = await Promise.all(
+  //         responses.flatMap((response) => response.attachments).map(async (image) => {
+  //           const res = await fetch(`${IP}/file/${image}`);
+  //           const imageBlob = await res.blob();
+  //           const imageUrl = URL.createObjectURL(imageBlob);
+  //           return { url: imageUrl, blob: imageBlob };
+  //         })
+  //       );
+
+  //       setImg(images);
+
+  //       const cachedData = JSON.stringify({ users: responses[0], images });
+  //       sessionStorage.setItem('cachedData', cachedData);
+
+  //       // Clean up object URLs
+  //       images.forEach((image) => {
+  //         URL.revokeObjectURL(image.url);
+  //       });
+  //     }
   //   }
-  // }, [dispatch, user, img , id]);
 
+  //   fetchData();
+  // }, []);
 
-
-
-  // const [ images, setImages]=useState([])
-  // const [img, setImg] = useState();
-
-  // const [user , setuser]=useState([])
-
-
-
-
-  // useEffect(() => {
-  //     fetch(`http://45.13.132.197:4000/api/post/fetch/${id}`).then((res) => {
-  //         return res.json();
-  //     }).then((data) => {
-  //         console.log("data", data)
-  //         setImages(data.attachments)
-  //         setuser(data)
-         
-  //     })
-  // }, [id])
-
-
-  
-  // useEffect(() => {
-  //   const fetchImage = async () => {
-  //     const res = await fetch(`http://45.13.132.197:4000/api/file/${images}`);
-  //     const imageBlob = await res.blob();
-  //     const imageObjectURL = URL.createObjectURL(imageBlob);
-  //     setImg(imageObjectURL);
-  //   };
-  //   fetchImage();
-  // }, [images]);
 
   const postIds = ['63f0cad81e627c34fc1b58e9'];
   const [users, setUsers] = useState([]);
-const [img, setImg] = useState('');
+  const [img, setImg] = useState('');
 
-useEffect(() => {
+  useEffect(() => {
     async function fetchData() {
       const responses = await Promise.all(
         postIds.map(async id => {
           const res = await fetch(`${IP}/post/fetch/${id}`);
           return res.json();
-          
+
         })
       );
       setUsers(responses[0]);
@@ -85,8 +77,47 @@ useEffect(() => {
     fetchData();
   }, [])
 
+  // const postIds = ['63f0cad81e627c34fc1b58e9'];
+  // const [users, setUsers] = useState([]);
+  // const [img, setImg] = useState('');
 
+  // useEffect(() => {
+  //   const cachedData = sessionStorage.getItem('cachedData');
 
+  //   if (cachedData) {
+  //     const parsedData = JSON.parse(cachedData);
+  //     setUsers(parsedData.users);
+  //     setImg(parsedData.images);
+  //   } else {
+  //     async function fetchData() {
+  //       const responses = await Promise.all(
+  //         postIds.map(async (id) => {
+  //           const res = await fetch(`${IP}/post/fetch/${id}`);
+  //           return res.json();
+  //         })
+  //       );
+
+  //       setUsers(responses[0]);
+
+  //       const images = await Promise.all(
+  //         responses.flatMap((response) => response.attachments).map(async (image) => {
+  //           const res = await fetch(`${IP}/file/${image}`);
+  //           const imageBlob = await res.blob();
+  //           return URL.createObjectURL(imageBlob);
+  //         })
+  //       );
+
+  //       setImg(images);
+
+  //       const cachedData = JSON.stringify({ users: responses[0], images });
+  //       sessionStorage.setItem('cachedData', cachedData);
+  //     }
+
+  //     fetchData();
+  //   }
+  // }, []);
+
+  // Rest of the code...
 
 
   return (
@@ -96,7 +127,7 @@ useEffect(() => {
           <div className="row">
             <div className="head">
               <h1>{users.title} <span>{users.excerpt}</span></h1>
-              <h3 dangerouslySetInnerHTML={{ __html: users.description }} style={{fontWeight:"500", fontSize:"15px"}}/>
+              <h3 dangerouslySetInnerHTML={{ __html: users.description }} style={{ fontWeight: "500", fontSize: "15px" }} />
               <button className="primary button small" type="button">
                 get started
               </button>
