8a5e9ea86f51a1bc7ca8ae52e044528f711bc1e6
munna tiysol <munna@tiysol.com>
2023-12-15 16:15:05 +0530

    add validation

--- src/Components/Pages/Reset_password/Reset_password.js
+++ src/Components/Pages/Reset_password/Reset_password.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import './Reset_password.css'; // Import your CSS file
 import { IP } from '../../../Constant';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 function ResetPassword() {
     const { tokenid } = useParams();
@@ -33,12 +36,26 @@ function ResetPassword() {
 
             const data = await response.json();
 
+
             if (response.ok) {
+
                 setMessage(data.message);
-                nav("/login");
+                // Show success notification and navigate to '/admin/Gift'
+                toast.success("Your password update successfully!", {
+                    position: "top-right",
+                    autoClose: 3000,
+                    onClose: () => {
+                        nav("/login");
+                    },
+                });
             } else {
                 setMessage(data.message || 'Failed to reset password');
+                // Show error notification if the API response is not successful
+
+
             }
+
+
         } catch (error) {
             console.error('Error during password reset:', error);
             setMessage('An error occurred during password reset');
@@ -48,31 +65,35 @@ function ResetPassword() {
 
 
     return (
-        <div className='reset-password-container'>
-            <h2>Reset Password</h2>
-            <div>
-                <label htmlFor="newPassword">New Password:</label>
-                <input
-                    type="password"
-                    id="newPassword"
-                    value={newPassword}
-                    required
-                    onChange={(e) => setNewPassword(e.target.value)}
-                />
+        <>
+            <div className='reset-password-container'>
+                <h2>Reset Password</h2>
+                <div>
+                    <label htmlFor="newPassword">New Password:</label>
+                    <input
+                        type="password"
+                        id="newPassword"
+                        value={newPassword}
+                        required
+                        onChange={(e) => setNewPassword(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="confirmPassword">Confirm Password:</label>
+                    <input
+                        type="password"
+                        id="confirmPassword"
+                        value={confirmPassword}
+                        required
+                        onChange={(e) => setConfirmPassword(e.target.value)}
+                    />
+                </div>
+                <button onClick={handleChangeNow} >Change Now</button>
+                <div>{message}</div>
             </div>
-            <div>
-                <label htmlFor="confirmPassword">Confirm Password:</label>
-                <input
-                    type="password"
-                    id="confirmPassword"
-                    value={confirmPassword}
-                    required
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                />
-            </div>
-            <button onClick={handleChangeNow} >Change Now</button>
-            <div>{message}</div>
-        </div>
+            <ToastContainer />
+        </>
+
     );
 }
 
