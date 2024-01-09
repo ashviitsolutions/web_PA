24076e313a29504d19a2a74006eae1fd6829523f
munna tiysol <munna@tiysol.com>
2024-01-09 16:38:04 +0530

    forget password

--- src/Components/Pages/Login/Extra.js
+++ src/Components/Pages/Login/Extra.js
@@ -1,104 +1,192 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from 'react'
 import "./Login.css"
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
-import axios from 'axios';
-function Login() {
-    const initialValues = {
+// import axios from 'axios';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { IP } from '../../../Constant';
 
+function Login() {
+    const [toggle, setToggle] = useState(false)
+    const loginguser = localStorage.getItem("token")
+    const nav = useNavigate()
+
+    useEffect(() => {
+        const auth = localStorage.getItem("token")
+        if (auth) {
+            nav('/userProfile')
+        }
+    })
+    // const nav=useNavigate()
+    const initialValues = {
         email: "",
         password: "",
 
     };
     const SignupSchema = Yup.object().shape({
         email: Yup.string().required("Required"),
-        password: Yup.string().required("Required"),
+        password: Yup.string().required("required")
+        // .required("required")
+        // .min(6, "atlist 8 character password is required")
+        // .matches(
+        //   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
+        //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
+        // ),
 
 
     });
-    const onSubmit = async (values, { setValues, resetForm }) => {
-        console.log(values);
-        alert("hii")
-        try {
-            const bodyFormData = new FormData();
-            bodyFormData.append("email", values.email);
-            bodyFormData.append("password", values.password);
 
-            const res = await axios.post("http://45.13.132.197:4000/login", bodyFormData);
-            console.log(res);
+
+    const onSubmit = async (values, { resetForm }) => {
+        console.log(values)
+
+
+        let data = { "email": values.email, "password": values.password }
+        try {
+            const resp = await fetch(`${IP}/user/login`, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            });
+            console.log(resp)
+            const token = resp.headers.get('Authorization');
+            const result = await resp.json();
+            console.log("result", result)
+
+
+
+            if (resp.status === 200) {
+                resetForm({ values: "" });
+                localStorage.setItem("users", JSON.stringify(result));
+                localStorage.setItem("userid", result?.user_info?._id);
+                localStorage.setItem("user_name", result?.user_info?.fullName);
+                localStorage.setItem("user_email", result?.user_info?.email);
+                localStorage.setItem("token", token);
+                nav("/userProfile")
+            } else {
+                setToggle(true)
+            }
 
         } catch (error) {
-            console.error(error);
+            console.log("Error show", error)
+
+        }
+
+
+
+    };
+
+
+
+    const forGet = async (values) => {
+        try {
+            const data = { "email": values.email };
+            console.log("email value", values.email);
+
+            const resp = await fetch(`${IP}/user/forgate-password`, {
+                method: "POST",
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data)
+            });
+
+            console.log(resp);
+
+            if (resp.status === 200) {
+                // Handle success, perhaps show a notification or redirect the user
+            } else {
+                // Handle failure, perhaps show an error message
+            }
+        } catch (error) {
+            console.log("Error:", error);
         }
     };
 
+
+
     return (
-        <>
+        <div id="login_page">
+            <div className="container">
+                <div className="row" style={{ textAlign: "center" }}>
+                    <Formik
+                        initialValues={initialValues}
+                        validationSchema={SignupSchema}
+                        onSubmit={onSubmit}
 
-            <div id="login_page">
-                <div className="container">
-                    <div className="row" style={{ textAlign: "center" }}>
+                    >
+                        {({ errors, touched, }) => (
+                            <Form className="sign_in_form">
+                                <div className="heading">
+                                    <h3 id='signtexxt'>Sign In</h3>
+                                </div>
+
+                                <div className="input_group">
+                                    <Field
+                                        className="input"
+                                        name="email"
+                                        type="email"
+                                        placeholder=""
+                                    />
+                                    {errors.email && touched.email ? (
+                                        <div>{errors.email}</div>
+                                    ) : null}
+                                    <label htmlFor="">E-mail</label>
+                                    <span className="highlight"></span>
+                                </div>
+                                <div style={{ height: "5px" }}>
+                                </div>
+                                <div className="input_group">
+                                    <Field
+                                        className="input"
+                                        name="password"
+                                        type="password"
+                                        placeholder=""
+                                    />
+                                    {errors.password && touched.password ? (
+                                        <div>{errors.password}</div>
+                                    ) : null}
+                                    <label htmlFor="">password</label>
+
+                                </div>
+                                <p style={{ background: 0, color: "#707070" }} >forget password ?</p>
+
+                                <div className="input_group" style={{ textDecoration: "none", paddingTop: "1px" }}>
+                                    <button className="button" type="submit"
+
+                                    >sign in</button>
+                                    <span >Don't have an account? <Link to="/sign_up" className="anchor" >Sign Up</Link> </span>
 
 
-                        <Formik
-                            initialValues={initialValues}
-                            validationSchema={SignupSchema}
-                            onSubmit={onSubmit}
-                        >
+                                    {
+                                        toggle ? (
+                                            <div id="notification_holder">
+                                                {!loginguser ? (
+                                                    <div className='notificatioerror'>
+                                                        <h3 id='errorshow'>Invalid credentials</h3>
+                                                    </div>
+                                                ) : (<h3>Success</h3>)
+                                                }
+                                            </div>
+                                        ) : null
+                                    }
+                                </div>
 
-                            {({ errors, touched, setFieldValue, values }) => (
 
-                                <Form className="sign_in_form">
-                                    <div className="heading">
-                                        <h3 id='signtexxt'>Sign In</h3>
-                                    </div>
-                                    <div className="input_group">
-                                        <Field
-                                            className="input"
-                                            name="email"
-                                            type="text"
-                                            placeholder=""
-                                        />
-                                        {errors.email && touched.email ? (
-                                            <div>{errors.email}</div>
-                                        ) : null}
-                                        <label htmlFor="">E-mail</label>
-                                        <span className="highlight"></span>
-                                    </div>
-                                    <div style={{ height: "5px" }}>
-                                    </div>
-                                    <div className="input_group">
-                                        <Field
-                                            className="input"
-                                            name="password"
-                                            type="text"
-                                            placeholder=""
-                                        />
-                                        {errors.password && touched.password ? (
-                                            <div>{errors.password}</div>
-                                        ) : null}
-                                        <label htmlFor="">password</label>
-                                        <span className="highlight"></span>
-                                    </div>
-                                    <Link to="#" style={{ background: 0, color: "#707070" }}>forget password ?</Link>
-                       
-                                        <div className="input_group">
-                                            <button type="submit" className="button">sign in</button>
-                                        </div>
-                
-                                    <span>Don't have an account? <Link to="/sign_up" className="anchor" style={{ textDecoration: "none" }}>Sign Up</Link> </span>
 
-                                    <button className="button gmail_login" > <span className="ico"></span> Sign In with Gmail</button>
-                                </Form>
-                            )}
+                            </Form>
+                        )}
+                    </Formik>
 
-                        </Formik>
 
-                    </div>
                 </div>
             </div>
-        </>
+        </div>
     )
 }
 
--- src/Components/Pages/Login/Login.js
+++ src/Components/Pages/Login/Login.js
@@ -1,160 +1,187 @@
-import React, { useEffect, useState } from 'react'
-import "./Login.css"
-import { Formik, Form, Field } from "formik";
-import * as Yup from "yup";
-// import axios from 'axios';
-import { Link } from 'react-router-dom';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import './Login.css';
+import * as Yup from 'yup';
+import { Link, useNavigate } from 'react-router-dom';
+import { IP } from '../../../Constant';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Login() {
-    const [toggle, setToggle] = useState(false)
-    const loginguser = localStorage.getItem("token")
-    const nav = useNavigate()
+    const [toggle, setToggle] = useState(false);
+    const [error, setError] = useState(false);
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const loginguser = localStorage.getItem('token');
+    const nav = useNavigate();
 
     useEffect(() => {
-        const auth = localStorage.getItem("token")
+        const auth = localStorage.getItem('token');
         if (auth) {
-            nav('/userProfile')
+            nav('/userProfile');
         }
-    })
-    // const nav=useNavigate()
+    }, [nav]);
+
     const initialValues = {
-        email: "",
-        password: "",
-
+        email: '',
+        password: '',
     };
+
     const SignupSchema = Yup.object().shape({
-        email: Yup.string().required("Required"),
-        password: Yup.string().required("required")
-        // .required("required")
-        // .min(6, "atlist 8 character password is required")
-        // .matches(
-        //   /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/,
-        //   "Must Contain 8 Characters, One Uppercase, One Lowercase, One Number and one special case Character"
-        // ),
-
-
+        email: Yup.string().required('Required'),
+        password: Yup.string().required('required'),
     });
-    const onSubmit = async (values, { resetForm }) => {
-        console.log(values)
 
-        
-        let data = { "email": values.email, "password": values.password }
+    const onSubmit = async () => {
+        const data = { email, password };
+
         try {
-            const resp = await fetch("http://45.13.132.197:5000/api/user/login", {
-                method: "POST",
+            const resp = await fetch(`${IP}/user/login`, {
+                method: 'POST',
                 headers: {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify(data)
+                body: JSON.stringify(data),
             });
-            console.log(resp)
+
             const token = resp.headers.get('Authorization');
             const result = await resp.json();
-            console.log("result" ,result)
-
-
 
             if (resp.status === 200) {
-                resetForm({ values: "" });
-                localStorage.setItem("users", JSON.stringify(result));
-                localStorage.setItem("userid", result?.user_info?._id);
-                localStorage.setItem("user_name", result?.user_info?.fullName);
-                localStorage.setItem("user_email", result?.user_info?.email);
-                localStorage.setItem("token", token);
-                nav("/userProfile")
+                localStorage.setItem('users', JSON.stringify(result));
+                localStorage.setItem('userid', result?.user_info?._id);
+                localStorage.setItem('user_name', result?.user_info?.fullName);
+                localStorage.setItem('user_email', result?.user_info?.email);
+                localStorage.setItem('token', token);
+                nav('/userProfile');
             } else {
-                setToggle(true)
+                setToggle(true);
             }
-
         } catch (error) {
-            console.log("Error show", error)
-
+            console.log('Error show', error);
         }
-
-
-
     };
-    
+
+    const forGet = async () => {
+        try {
+            const data = { email };
+            const resp = await fetch(`${IP}/user/forgate-password`, {
+                method: 'POST',
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(data),
+            });
+
+            if (resp.status === 200) {
+                const result = await resp.json();
+                console.log("result", result);
+
+                toast.success("Password reset email sent. Check your inbox.", {
+                    position: "top-right",
+                    autoClose: 3000,
+                    onClose: () => {
+                        nav("/login");
+                    },
+                });
+
+                // Handle success, perhaps show a notification or redirect the user
+            } else {
+                // Handle failure, perhaps show an error message
+                const errorResult = await resp.json();
+                toast.error(`${errorResult.message}`, {
+                    position: "top-right",
+                    autoClose: 3000,
+                    onClose: () => {
+                        nav("/login");
+                    },
+                });
+                console.log("errorResult", errorResult);
+            }
+        } catch (error) {
+            console.log('Error:', error);
+            toast.error("An error occurred. Please try again.", {
+                position: "top-right",
+                autoClose: 3000,
+            });
+        }
+    };
+
+
+
 
     return (
-        <div id="login_page">
-            <div className="container">
-                <div className="row" style={{ textAlign: "center" }}>
-                    <Formik
-                        initialValues={initialValues}
-                        validationSchema={SignupSchema}
-                        onSubmit={onSubmit}
-                    >
-                        {({ errors, touched, }) => (
-                            <Form className="sign_in_form">
-                                <div className="heading">
-                                    <h3 id='signtexxt'>Sign In</h3>
-                                </div>
+        <>
+            <div id="login_page">
+                <div className="container">
+                    <div className="row" style={{ textAlign: 'center' }}>
+                        <div className="sign_in_form">
+                            <div className="heading">
+                                <h3 id="signtexxt">Sign In</h3>
+                            </div>
 
-                                <div className="input_group">
-                                    <Field
-                                        className="input"
-                                        name="email"
-                                        type="email"
-                                        placeholder=""
-                                    />
-                                    {errors.email && touched.email ? (
-                                        <div>{errors.email}</div>
-                                    ) : null}
-                                    <label htmlFor="">E-mail</label>
-                                    <span className="highlight"></span>
-                                </div>
-                                <div style={{ height: "5px" }}>
-                                </div>
-                                <div className="input_group">
-                                    <Field
-                                        className="input"
-                                        name="password"
-                                        type="password"
-                                        placeholder=""
-                                    />
-                                    {errors.password && touched.password ? (
-                                        <div>{errors.password}</div>
-                                    ) : null}
-                                    <label htmlFor="">password</label>
+                            <div className="input_group">
+                                <input
+                                    className="input"
+                                    name="email"
+                                    type="email"
+                                    placeholder=""
+                                    value={email}
+                                    onChange={(e) => setEmail(e.target.value)}
+                                />
+                                <label htmlFor="">E-mail</label>
+                                <span className="highlight"></span>
+                            </div>
+                            <div style={{ height: '5px' }}></div>
+                            <div className="input_group">
+                                <input
+                                    className="input"
+                                    name="password"
+                                    type="password"
+                                    placeholder=""
+                                    value={password}
+                                    onChange={(e) => setPassword(e.target.value)}
+                                />
+                                <label htmlFor="">password</label>
+                            </div>
+                            <p style={{ background: 0, color: '#707070', cursor: "pointer" }} onClick={forGet}>
+                                forget password ?
+                            </p>
 
-                                </div>
-                                <Link to="#" style={{ background: 0, color: "#707070" }}>forget password ?</Link>
+                            <div className="input_group" style={{ textDecoration: 'none', paddingTop: '1px' }}>
+                                <button className="button" type="button" onClick={onSubmit}>
+                                    sign in
+                                </button>
+                                <span>
+                                    Don't have an account? <Link to="/sign_up" className="anchor">
+                                        Sign Up
+                                    </Link>{' '}
+                                </span>
 
-                                <div className="input_group" style={{ textDecoration: "none", paddingTop: "1px" }}>
-                                    <button className="button" type="submit"
+                                {toggle ? (
+                                    <div id="notification_holder">
+                                        {!loginguser ? (
+                                            <div className="notificatioerror">
+                                                <h3 id="errorshow">Invalid credentials</h3>
 
-                                    >sign in</button>
-                                    <span >Don't have an account? <Link to="/sign_up" className="anchor" >Sign Up</Link> </span>
-
-                                   
-                                    {
-                                        toggle ? (
-                                            <div id="notification_holder">
-                                                {!loginguser ? (
-                                                    <div className='notificatioerror'>
-                                                        <h3 id='errorshow'>Invalid credentials</h3>
-                                                    </div>
-                                                ) : (<h3>Success</h3>)
-                                                }
                                             </div>
-                                        ) : null
-                                    }
-                                </div>
-
-
-
-                            </Form>
-                        )}
-                    </Formik>
-
-
+                                        ) : (
+                                            <h3>Success</h3>
+                                        )}
+                                    </div>
+                                ) : null}
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+
+            <ToastContainer />
+        </>
+
+
+    );
 }
 
-export default Login
+export default Login;
