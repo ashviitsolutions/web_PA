904aaf62d1caeb6604f23591c30a24feafac7d38
Munna Kumar <munnakr686622@gmail.com>
2024-03-08 18:08:07 +0530

    update

--- src/Components/Pages/Profile/userProfile/Profile.js
+++ src/Components/Pages/Profile/userProfile/Profile.js
@@ -1,9 +1,360 @@
-import React from 'react'
+import React, { useState } from "react";
+import FirstForm from "./Overview";
+import SecondForm from "./Booking";
+import ThirdForm from "./Invoices";
+import FourForm from "./Event";
+import FifthForm from "./Gift";
+import Conform from "./Setting";
+import Logout from "./Logout";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import bookingimage from "../../../img/plus.png";
+import Membership from "./Membership";
 
-function Profile() {
+import "./Profile.css";
+import image1 from "../../../assets/img/profile/dashboard.png";
+import image2 from "../../../assets/img/profile/calendar.png";
+import image3 from "../../../assets/img/profile/invoice.png";
+import image4 from "../../../assets/img/profile/election-event-on-a-calendar-with-star-symbol.png";
+import image5 from "../../../assets/img/profile/gift-card.png";
+
+import image6 from "../../../assets/img/profile/settings.png";
+import image7 from "../../../assets/img/profile/exit.png";
+import menuimage from "../../../assets/img/close.png";
+import menucross from "../../../assets/img/menu.png";
+import "./Profile.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faArrowRightFromBracket,
+	faBell,
+	faBorderAll,
+	faCalendarDays,
+	faFileContract,
+	faGift,
+	faHeadset,
+	faHeart,
+	faIdBadge,
+	faMoneyCheckDollar,
+	faRectangleXmark,
+	faSpa,
+} from "@fortawesome/free-solid-svg-icons";
+import Notifications from "./Notifications";
+import Favorites from "./Favorites";
+import Support from "./Support";
+
+const Profile = () => {
+	const [activeTab, setActiveTab] = useState(1);
+	const [now, setNow] = useState(0);
+	const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+	const nextStep = () => {
+		setNow((prevStep) => (prevStep < 100 ? prevStep + 100 / 7 : prevStep));
+		setActiveTab(activeTab + 1);
+	};
+
+	const previousStep = () => {
+		setNow((prevStep) => (prevStep > 0 ? prevStep - 100 / 7 : prevStep));
+	};
+
+	const nav = useNavigate();
+
+	const handleLogout = () => {
+		localStorage.clear();
+		nav("/");
+	};
+
+	let form;
+	switch (activeTab) {
+		case 1:
+			form = (
+				<FirstForm step={now} nextStep={nextStep} previousStep={previousStep} />
+			);
+			break;
+		case 2:
+			form = (
+				<SecondForm
+					step={now}
+					nextStep={nextStep}
+					previousStep={previousStep}
+				/>
+			);
+			break;
+		case 3:
+			form = (
+				<ThirdForm step={now} nextStep={nextStep} previousStep={previousStep} />
+			);
+			break;
+		case 4:
+			form = (
+				<Membership
+					step={now}
+					nextStep={nextStep}
+					previousStep={previousStep}
+				/>
+			);
+			break;
+		case 5:
+			form = (
+				<FifthForm step={now} nextStep={nextStep} previousStep={previousStep} />
+			);
+			break;
+		case 6:
+			form = (
+				<Conform step={now} nextStep={nextStep} previousStep={previousStep} />
+			);
+			break;
+		case 7:
+			form = (
+				<Logout step={now} nextStep={nextStep} previousStep={previousStep} />
+			);
+			break;
+		case 8:
+			form = <Notifications />;
+			break;
+		case 9:
+			form = <Favorites />;
+			break;
+		case 10:
+			form = <Support />;
+			break;
+		default:
+			break;
+	}
+
+	const handleSidebarToggle = () => {
+		setIsSidebarOpen((prevState) => !prevState);
+	};
+	// const toggleLinks = () => {
+	//   setIsActive((prevState) => !prevState);
+	// };
+	const handleBook = () => {
+		nav("/select_location");
+	};
+	const navigate = useNavigate();
 	return (
-		<div>Profile</div>
-	)
-}
+		<div className="parent">
+			{/* <div className={`sidebar_tab ${isSidebarOpen ? "mobile-view" : ""}`}>
+			 */}
+			<div className={`sidebar_tab ${isSidebarOpen && "mobile-view"}`}>
+				<div className="">
+					<ul id="tabs_control">
+						<li
+							id="tab_1"
+							className={activeTab === 1 && "active"}
+							onClick={() => setActiveTab(1)}
+						>
+							<div className="item" onClick={handleSidebarToggle}>
+								{/* <img
+								src={image1}
+								width={15}
+								height={15}
+								alt="..."
+								className={`${activeTab == 1 && "active__Img"}`}
+							/> */}
+								<FontAwesomeIcon
+									icon={faBorderAll}
+									style={{ marginRight: 10 }}
+								/>
+								Overview
+							</div>
+						</li>
+						<li
+							id="tab_2"
+							className={activeTab === 2 ? "active" : ""}
+							onClick={() => setActiveTab(2)}
+						>
+							<div className="item" onClick={handleSidebarToggle}>
+								{/* <img src={image2} width={15} height={15} alt="..." /> */}
+								<FontAwesomeIcon
+									icon={faCalendarDays}
+									style={{ marginRight: 10 }}
+								/>
+								Reservations
+							</div>
+						</li>
+						{/* <li id="tab_3" className={activeTab === 3 ? 'active' : ''} onClick={() => setActiveTab(3)}>
+            <div className="item">
+              <img src={image3} width={15} height={15} alt="..." />
+              Invoices
+            </div>
+          </li> */}
+						<li
+							id="tab_4"
+							className={activeTab === 4 ? "active" : ""}
+							onClick={() => setActiveTab(4)}
+						>
+							<div className="item" onClick={handleSidebarToggle}>
+								{/* <img src={image4} width={15} height={15} alt="..." /> */}
+								<FontAwesomeIcon
+									icon={faMoneyCheckDollar}
+									style={{ marginRight: 10 }}
+								/>
+								Membership
+							</div>
+						</li>
+						<li
+							id="tab_5"
+							className={activeTab === 5 ? "active" : ""}
+							onClick={() => setActiveTab(5)}
+						>
+							<div className="item" onClick={handleSidebarToggle}>
+								{/* <img src={image5} width={15} height={15} alt="..." /> */}
+								<FontAwesomeIcon
+									icon={faGift}
+									size={15}
+									style={{ marginRight: 10 }}
+								/>
+								Gift Card
+							</div>
+						</li>
+						<li
+							id="tab_6"
+							className={activeTab === 6 ? "active" : ""}
+							onClick={() => setActiveTab(6)}
+						>
+							<div className="item" onClick={handleSidebarToggle}>
+								{/* <img src={image6} width={15} height={15} alt="..." />
+								 */}
+								<FontAwesomeIcon icon={faIdBadge} style={{ marginRight: 10 }} />
+								Profile
+							</div>
+						</li>
+						<li
+							id=""
+							className={activeTab === 8 ? "active" : ""}
+							onClick={() => setActiveTab(8)}
+						>
+							<div className="item" onClick={handleSidebarToggle}>
+								{/* <img src={image7} width={15} height={15} alt="..." /> */}
+								<FontAwesomeIcon icon={faBell} style={{ marginRight: 10 }} />
+								Notifications
+							</div>
+						</li>
+						<li
+							id=""
+							className={activeTab === 9 ? "active" : ""}
+							onClick={() => setActiveTab(9)}
+						>
+							<div className="item" onClick={handleSidebarToggle}>
+								{/* <img src={image7} width={15} height={15} alt="..." /> */}
+								<FontAwesomeIcon icon={faHeart} style={{ marginRight: 10 }} />
+								Favorites
+							</div>
+						</li>
+						<li
+							id=""
+							className={activeTab === 10 ? "active" : ""}
+							onClick={() => setActiveTab(10)}
+						>
+							<div className="item" onClick={handleSidebarToggle}>
+								{/* <img src={image7} width={15} height={15} alt="..." /> */}
+								<FontAwesomeIcon icon={faHeadset} style={{ marginRight: 10 }} />
+								Support
+							</div>
+						</li>
+						<li id="tab_7" className={activeTab === 7 ? "active" : ""}>
+							<div className="item" onClick={handleLogout}>
+								{/* <img src={image7} width={15} height={15} alt="..." /> */}
+								<FontAwesomeIcon
+									icon={faArrowRightFromBracket}
+									style={{ marginRight: 10 }}
+								/>
+								Logout
+							</div>
+						</li>
 
-export default Profile
+						<div className="userbooking">
+							<li id="tab_7">
+								<div className="item" onClick={handleBook}>
+									{/* <img src={image7} width={15} height={15} alt="..." /> */}
+									<FontAwesomeIcon icon={faSpa} style={{ marginRight: 10 }} />
+									Book now
+								</div>
+							</li>
+						</div>
+					</ul>
+					<div id="navigationuser">
+						<div className="toggle_buttons" onClick={handleSidebarToggle}>
+							{isSidebarOpen ? (
+								<img src={menuimage} alt="Close" className="toggleimages" />
+							) : (
+								<img src={menucross} alt="Menu" className="toggleimages" />
+							)}
+						</div>
+					</div>
+				</div>
+				<div
+					className="
+        "
+				>
+					{/* <ul id="tabs_control">
+						<li id="">
+							<div
+								className="item"
+								style={{ fontSize: 10 }}
+								onClick={() => navigate("/termcondition")}
+							>
+								
+								<FontAwesomeIcon
+									icon={faFileContract}
+									style={{ marginRight: 10 }}
+								/>
+								Terms and Conditions
+							</div>
+						</li>
+
+						<li id="">
+							<div
+								className="item"
+								style={{ fontSize: 10 }}
+								onClick={() => navigate("/cancelationpolicy")}
+							>
+								
+								<FontAwesomeIcon
+									icon={faRectangleXmark}
+									style={{ marginRight: 10 }}
+								/>
+								Cancellation Policy
+							</div>
+						</li>
+					</ul> */}
+				</div>
+			</div>
+
+			{/*Booking Image */}
+
+			<div id="imagebooking">
+				<img
+					width={40}
+					height={40}
+					src={bookingimage}
+					alt=""
+					style={{
+						borderRadius: "100%",
+						display: "block",
+						boxShadow: "4px 4px 4px 0px rgb(6 77 109)", // Correct syntax
+					}}
+					className="profileiconimage"
+					onClick={() => nav("/guest_login")}
+				/>
+			</div>
+			{/* Toggle button for mobile view */}
+
+			<div className="progressbar_userpannel profileSpace">
+				{form}
+				<div className="user_profile_footer">
+					<div className="footer_container">
+						<div className="footer_warapper">
+							<p className="vsmall">Copyright © 2021 productive alliance, All Rights Reserved. <span className="cursor" onClick={() => navigate("/cancelationpolicy")}>Cancellation Policy.</span> <span className="cursor" onClick={() => navigate("/termcondition")}>Terms and conditions.</span></p>
+						</div>
+						<div className="footer_warapper">
+							<p id="ashvi_design" className="vsmall">A design by Ashvi IT solution</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Profile;
