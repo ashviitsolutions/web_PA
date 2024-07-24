1d48ebd6fd6ed0639b4288cbfcf10bbe6772d5db
munna <munna572000>
2024-07-24 21:17:29 +0530

    update chnages

--- src/PROVIDER/layouts/EarningCard.js
+++ src/PROVIDER/layouts/EarningCard.js
@@ -88,90 +88,91 @@ function EarningCard() {
     return (
         <>
 
-          
 
 
 
-                    <div className="row">
-                        <div className="gutter">
-                            <table className="table-responsive ultra_responsive">
-                                <thead>
-                                    <tr>
-                                        <th>Payment Proof</th>
-                                        <th>Description</th>
-                                        <th>Amount</th>
-                                        <th>View Details</th>
+
+            <div className="row">
+                <div className="gutter">
+                    <table className="table-responsive ultra_responsive">
+                        <thead>
+                            <tr>
+                                <th>Payment Proof</th>
+                                <th>Description</th>
+                                <th>Amount</th>
+                                <th>PaymentId</th>
+                            </tr>
+                        </thead>
+
+
+                        {memoizedUser.map((cur, index) => (
+                            <React.Fragment key={index}>
+                                <tbody id="post_container">
+                                    <tr className="wrapper" id="tr_post_77">
+                                        <td>
+                                            <div className="avatar_wrap">
+                                                <div className="inner">
+                                                    <div className='preview' style={{
+                                                        width: "50%",
+                                                        //  height:"80px",
+                                                        backgroundSize: "cover",
+
+                                                    }}>
+                                                        <PreviewImage className="avatar" attachments={cur.attachments} />
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div className="content">
+                                                <span className="title" >{`${cur.additionalInfo}`} </span>
+                                                <span className="title" style={{ display: "block", fontSize: "12px" }}>{cur.email}</span>
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div className="content">
+                                                <span className="title">{cur?.amount?.toFixed(2)}</span>
+
+                                            </div>
+                                        </td>
+                                        <td>
+                                            <div className="content">
+                                                <span className="title">{cur?.paymentId}</span>
+
+                                            </div>
+                                        </td>
                                     </tr>
-                                </thead>
+                                </tbody>
+                            </React.Fragment>
+                        ))}
 
+                    </table>
+                </div>
+                <div className='pagination'  >
+                    <ReactPaginate
+                        itemsPerPage={10}
+                        previousLabel={'Previous'}
+                        nextLabel={'Next'}
+                        breakLabel={"..."}
+                        pageCount={Math.ceil(count / 10)}
+                        marginPagesDisplayed={3}
+                        pageRangeDisplayed={2}
+                        onPageChange={handlePageClick}
+                        // css apply on pagination
+                        containerClassName={'pagination justify-content-center py-3'}
+                        pageClassName={'page-item'}
+                        pageLinkClassName={'page-link'}
+                        previousClassName={'page-item'}
+                        previousLinkClassName={'page-link'}
+                        nextClassName={'page-item'}
+                        nextLinkClassName={'page-link'}
+                        breakClassName={'page-item'}
+                        breakLinkClassName={'page-link'}
+                        activeClassName={'active'}
+                    />
+                </div>
+            </div>
 
-                                {memoizedUser.map((cur, index) => (
-                                    <React.Fragment key={index}>
-                                        <tbody id="post_container">
-                                            <tr className="wrapper" id="tr_post_77">
-                                                <td>
-                                                    <div className="avatar_wrap">
-                                                        <div className="inner">
-                                                            <div className='preview' style={{
-                                                                width: "50%",
-                                                                //  height:"80px",
-                                                                backgroundSize: "cover",
-
-                                                            }}>
-                                                                <PreviewImage className="avatar" attachments={cur.attachments} />
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <div className="content">
-                                                        <span className="title" >{`${cur.additionalInfo}`} </span>
-                                                        <span className="title" style={{ display: "block", fontSize: "12px" }}>{cur.email}</span>
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <div className="content">
-                                                        <span className="title">{cur.amount}</span>
-
-                                                    </div>
-                                                </td>
-                                                <td>
-                                                    <Link to="#" >
-                                                        <RemoveRedEyeIcon />
-                                                    </Link>
-                                                </td>
-                                            </tr>
-                                        </tbody>
-                                    </React.Fragment>
-                                ))}
-
-                            </table>
-                        </div>
-                        <div className='pagination'  >
-                            <ReactPaginate
-                                itemsPerPage={10}
-                                previousLabel={'Previous'}
-                                nextLabel={'Next'}
-                                breakLabel={"..."}
-                                pageCount={Math.ceil(count / 10)}
-                                marginPagesDisplayed={3}
-                                pageRangeDisplayed={2}
-                                onPageChange={handlePageClick}
-                                // css apply on pagination
-                                containerClassName={'pagination justify-content-center py-3'}
-                                pageClassName={'page-item'}
-                                pageLinkClassName={'page-link'}
-                                previousClassName={'page-item'}
-                                previousLinkClassName={'page-link'}
-                                nextClassName={'page-item'}
-                                nextLinkClassName={'page-link'}
-                                breakClassName={'page-item'}
-                                breakLinkClassName={'page-link'}
-                                activeClassName={'active'}
-                            />
-                        </div>
-                    </div>
-          
         </>
     )
 }
