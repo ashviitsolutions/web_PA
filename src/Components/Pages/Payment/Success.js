b6c36c884e8688686d1cebcbae8e51b2c888699a
munna <munna572000>
2024-07-26 19:01:58 +0530

    update

--- src/Components/Pages/Payment/Success.js
+++ src/Components/Pages/Payment/Success.js
@@ -12,51 +12,60 @@ function Success() {
   const searchParams = new URLSearchParams(location.search);
   const session_id = searchParams.get('session_id');
   const tokenuser = localStorage.getItem("token");
-  
+
   const [session, setSession] = useState(session_id);
   const [token, setToken] = useState(tokenuser);
 
-  useEffect(() => {
-    setSession(session_id);
-    setToken(tokenuser);
-  }, [session_id, tokenuser]);
+  // useEffect(() => {
+  //   setSession(session_id);
+  //   setToken(tokenuser);
+  // }, [session_id, tokenuser]);
+
+  // useEffect(() => {
+  //   const onSubmit = async () => {
+  //     if (!session || !token) return;
+
+  //     try {
+  //       const url = `${IP}/user/service_bookssssss?session_id=${session}`;
+  //       const config = {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //           Authorization: token,
+  //           // 'Cache-Control': 'no-cache', 
+  //         },
+  //       };
+  //       const res = await axios.get(url, config);
+
+  //       console.log("respose of booking", res?.status)
+
+  //       if (res.status === 200) {
+  //         // navigate('/userProfile'); 
+  //       } else {
+  //         console.error('Failed to process payment:', res.data.error);
+  //         // Optionally, navigate to an error page or show a message
+  //       }
+  //     } catch (error) {
+  //       console.error('Error processing payment:', error);
+  //       // Optionally, navigate to a cancel/error page or show a message
+  //     }
+  //   };
+
+  //   const timer = setTimeout(() => {
+  //     onSubmit();
+  //   }, 2000);
+
+  //   // Clean up the timer if the component unmounts before the timeout
+  //   return () => clearTimeout(timer);
+  // }, [session, token, navigate]);
+
 
   useEffect(() => {
-    const onSubmit = async () => {
-      if (!session || !token) return;
-
-      try {
-        const url = `${IP}/user/service_book?session_id=${session}`;
-        const config = {
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: token,
-            // 'Cache-Control': 'no-cache', 
-          },
-        };
-        const res = await axios.get(url, config);
-
-        console.log("respose of booking", res?.status)
-
-        if (res.status === 200) {
-          navigate('/userProfile'); 
-        } else {
-          console.error('Failed to process payment:', res.data.error);
-          // Optionally, navigate to an error page or show a message
-        }
-      } catch (error) {
-        console.error('Error processing payment:', error);
-        // Optionally, navigate to a cancel/error page or show a message
-      }
-    };
-
     const timer = setTimeout(() => {
-      onSubmit();
-    }, 2000);
-
+      navigate('/userProfile');
+    }, 3000);
     // Clean up the timer if the component unmounts before the timeout
     return () => clearTimeout(timer);
-  }, [session, token, navigate]);
+  }, [])
 
   return (
     <div className='PaymentForm'>
